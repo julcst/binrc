ce357862d990eb1cf629a4c8b1a85bfec6666928
Julcs <mail@julcs.com>
2025-06-26 18:29:25 +0200

    Fix BTDF

--- src/optix/principled_brdf.cuh
+++ src/optix/principled_brdf.cuh
@@ -316,9 +316,9 @@ __device__ constexpr BRDFResult evalDisney(const float3& wo, const float3& wi, c
         pdf += weights.diffuse * pdfDiffuse;
     } else if (weights.transmission > 0.0f) {
         // Transmission
-        const auto transmission = mat.albedo * (1.0f - F) * safediv(mat.transmission * D * abs(HdotL * HdotV), max(invG * NdotV * NdotL * pow2(HdotL + HdotV / eta), 1e-3f)); // Prevent numerical artifacts
+        const auto transmission = mat.albedo * (1.0f - F) * safediv(mat.transmission * D * abs(HdotL * HdotV), max(invG * NdotV * pow2(HdotL + HdotV / eta), 1e-3f)); // NOTE: NdotL cancels with the denominator
         throughput += transmission;
-        const auto pdfTransmission = safediv(D, invG1 * pow2(HdotL + HdotV / eta));
+        const auto pdfTransmission = safediv(D * HdotV * HdotV, NdotV * invG1 * pow2(HdotL + HdotV / eta));
         pdf += weights.transmission * pdfTransmission;
     }
 
@@ -369,7 +369,7 @@ __device__ constexpr float3 evalDisneyBRDFOnly(const float3& wo, const float3& w
         throughput += diffuse;
     } else if (mat.transmission > 0.0f) {
         // Transmission
-        const auto transmission = mat.albedo * (1.0f - F) * safediv(mat.transmission * D * abs(HdotL * HdotV), max(invG * NdotV * NdotL * pow2(HdotL + HdotV / eta), 1e-3f)); // Prevent numerical artifacts
+        const auto transmission = mat.albedo * (1.0f - F) * safediv(mat.transmission * D * abs(HdotL * HdotV), max(invG * NdotV * pow2(HdotL + HdotV / eta), 1e-3f)); // NOTE: NdotL cancels with the denominator
         throughput += transmission;
     }
 
@@ -448,11 +448,12 @@ __device__ constexpr BRDFResult evalDisneyWeighted(const float3& wo, const float
         // if (isnegative(pdfDiffuse)) printf("pdfDiffuse is negative: %f %f %f %f %f\n", pdfDiffuse, NdotV, NdotL, lambdaL, lambdaV);
     } else if (weights.transmission > 0.0f) {
         // Transmission
-        // const auto transmission = albedo * (1.0f - F) * safediv(transmissiveness * D * G * abs(HdotL * HdotV), NdotV * NdotL * pow2(HdotL + HdotV / eta));
-        const auto transmission = mat.albedo * (1.0f - F) * mat.transmission * invG1 / (invG * weights.transmission); // TODO: Handle dirac
+        // const auto transmission = mat.albedo * (1.0f - F) * safediv(mat.transmission * D * abs(HdotL * HdotV), invG * NdotV * pow2(HdotL + HdotV / eta)); // NOTE: NdotL cancels with the denominator
+        const auto transmission = mat.albedo * (1.0f - F) * safediv(mat.transmission * invG1 * HdotL, HdotV * invG * weights.transmission);
         throughput = transmission;
         // NOTE: D_VNDF = D * G1 * HdotV / NdotV
-        const auto pdfTransmission = safediv(D, invG1 * pow2(HdotL + HdotV / eta)); // Missing * abs(HdotL) ?
+        // const auto pdfTransmission = safediv(D, invG1 * pow2(HdotL + HdotV / eta)); // Missing * abs(HdotL) ?
+        const auto pdfTransmission = safediv(D * HdotV * HdotV, NdotV * invG1 * pow2(HdotL + HdotV / eta));
         pdf = weights.transmission * pdfTransmission;
 
         // if (luminance(transmission) > 2.0f) printf("Transmission is larger than 2: %f %f %f %f %f %f %f %f %f %f %f %f\n", transmission.x, transmission.y, transmission.z, pdfTransmission, dot(n, wi), dot(n, wo), HdotL, HdotV, NdotH, eta, 1.0f / invG1, D);
